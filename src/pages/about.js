6f2e48c0cee8850943c07dbc544fc8b888c7af41
Daniel Lousa <dlousa92@gmail.com>
2018-10-27 14:51:11 -0400

    initial commits

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Header from '../components/Header'
+
+export default () => (
+  <div style={{ color: 'teal' }}>
+    <Header headerText='About Gatsby' />
+    <Header headerText='Its pretty cool' />
+    <p>Such wow. Very React</p>
+  </div>
+)
